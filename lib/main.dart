cd04405926d88223581cc0bb39cfbcc2b5c01309
Danushan99 <danuthasan08@gmail.com>
2025-06-02 09:58:20 +0530

    feat: implement shopping cart functionality with product and cart models; add UI for cart and home screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,42 @@
-import 'package:concentric_transition/page_view.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:ui_migration/pages/animated_container.dart';
-import 'package:ui_migration/pages/awesome_dropdown_example.dart';
-import 'package:ui_migration/pages/chart_example.dart';
-import 'package:ui_migration/pages/concentric_transition.dart';
-import 'package:ui_migration/pages/design_tools.dart';
-import 'package:ui_migration/pages/discover.dart';
-import 'package:ui_migration/pages/dream_job.dart';
-import 'package:ui_migration/pages/expenses.dart';
-import 'package:ui_migration/pages/github_users.dart';
-import 'package:ui_migration/pages/learn_flutter.dart';
-import 'package:ui_migration/pages/loading.dart';
-import 'package:ui_migration/pages/movies.dart';
-import 'package:ui_migration/pages/new_collection.dart';
-import 'package:ui_migration/pages/no_connection.dart';
-import 'package:ui_migration/pages/product_card.dart';
-import 'package:ui_migration/pages/spinner_page.dart';
-import 'package:ui_migration/pages/squid_game.dart';
-import 'package:ui_migration/pages/staggered_gridview.dart';
-import 'package:ui_migration/pages/tea_page.dart';
-import 'package:ui_migration/pages/vartical_card_pager.dart';
-import 'package:ui_migration/pages/verification.dart';
-import 'package:ui_migration/pages/vero.dart';
+import 'package:ui_migration/shop/provider/cart_provider.dart';
+import 'package:ui_migration/shop/provider/product_provider.dart';
+import 'package:ui_migration/shop/screens/cart_screen.dart';
+import 'package:ui_migration/shop/screens/home_screen.dart';
 
-void main() => runApp(const MaterialApp(
+// void main() => runApp(const MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: VeroPage(),
+//     ));
+
+void main() {
+  runApp(MultiProvider(providers: [
+    ChangeNotifierProvider(
+      create: (_) => CartProvider(),
+    ),
+    ChangeNotifierProvider(
+      create: (_) => ProductProvider(),
+    ),
+  ], child: const MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {
+        '/home': (context) => const HomeScreen(),
+        '/cart': (context) => const CartScreen(),
+      },
       debugShowCheckedModeBanner: false,
-      home: VeroPage(),
-    ));
+      initialRoute: '/home',
+    );
+  }
+}
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
---
+++ lib/shop/models/cart_model.dart
@@ -0,0 +1,15 @@
+
+import 'package:ui_migration/shop/models/product_model.dart';
+import 'package:uuid/uuid.dart';
+
+var uuid = Uuid();
+
+class CartModel {
+  final String id;
+  final ProductModel product;
+  int quantity;
+  CartModel({
+    required this.product,
+    required this.quantity,
+  }) : id = uuid.v4();
+}
