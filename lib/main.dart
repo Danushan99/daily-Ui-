5467ee85d8d60920153ea38b11d9d9ad771115f7
Danushan99 <danuthasan08@gmail.com>
2025-06-03 21:32:04 +0530

    refactor: clean up main.dart by commenting out unused code and organizing imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:ui_migration/pages/animated_container.dart';
+import 'package:ui_migration/pages/chart_example.dart';
+import 'package:ui_migration/pages/stocks.dart';
+import 'package:ui_migration/pages/verification.dart';
 import 'package:ui_migration/shop/provider/cart_provider.dart';
 import 'package:ui_migration/shop/provider/product_provider.dart';
 import 'package:ui_migration/shop/screens/cart_screen.dart';
 import 'package:ui_migration/shop/screens/home_screen.dart';
 
-// void main() => runApp(const MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       home: VeroPage(),
-//     ));
-void main() {
-  runApp(MultiProvider(providers: [
-    ChangeNotifierProvider(
-      create: (_) => CartProvider(),
-    ),
-    ChangeNotifierProvider(
-      create: (_) => ProductProvider(),
-    ),
-  ], child: const MyApp()));
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      routes: {
-        '/home': (context) => const HomeScreen(),
-        '/cart': (context) => const CartScreen(),
-      },
+void main() => runApp(const MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: '/home',
-    );
-  }
-}
+      home: Verificatoin(),
+    ));
+// void main() {
+//   runApp(MultiProvider(providers: [
+//     ChangeNotifierProvider(
+//       create: (_) => CartProvider(),
+//     ),
+//     ChangeNotifierProvider(
+//       create: (_) => ProductProvider(),
+//     ),
+//   ], child: const ChartExample()));
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       routes: {
+//         '/home': (context) => const HomeScreen(),
+//         '/cart': (context) => const CartScreen(),
+//       },
+//       debugShowCheckedModeBanner: false,
+//       initialRoute: '/home',
+//     );
+//   }
+// }
+
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
