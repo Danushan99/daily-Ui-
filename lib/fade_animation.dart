4cf396c71aa368a56b61a2e5ccb865e31501abe3
Danushan99 <danuthasan08@gmail.com>
2025-06-13 21:32:22 +0530

    fix: update FadeAnimation to use const constructors for improved performance

--- lib/fade_animation.dart
+++ lib/fade_animation.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: library_private_types_in_public_api
+
 import 'package:flutter/material.dart';
 import 'package:ui_migration/pages/button.dart';
 
@@ -15,20 +17,20 @@ class _FadeAnimationState extends State<FadeAnimation> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SizedBox(
       height: 300,
       child: Column(
         children: [
           AnimatedOpacity(
             opacity: _visible ? 1 : 0, 
-            duration: Duration(milliseconds: 500),
+            duration: const Duration(milliseconds: 500),
             child: Container(
               width: 200,
               height: 100,
               color: Colors.orangeAccent,
             ),
           ),
-          SizedBox(height: 40),
+          const SizedBox(height: 40),
           BorderedButton(text: "Show/Hide", onPress: () {
             setState(() {
               _visible = !_visible;
